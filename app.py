8bbdfdd7e2ba797b6ab7e74ec6d762a6a82849d7
adi04092002 <adityaraut95@gmail.com>
2026-02-14 14:20:41 +0530

    Valentine website 1.2

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", name="Urvi")
+    image_folder = "static/images/photos"
+    images = os.listdir(image_folder)
+    images = [f"/{image_folder}/{img}" for img in images if img.lower().endswith(('.jpg', '.png', '.jpeg'))]
+
+    return render_template(
+        "index.html",
+        name="Urvi",
+        images=images
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
